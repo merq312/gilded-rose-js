c5295996d304179ebe15e044fd6630edc52ebb8c
Chamila Gunasena <merc312@gmail.com>
2021-10-27 21:20:45 -0400

    add prettier and format

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,12 +1,12 @@
-const { Shop, Item } = require("../src/gilded_rose");
+const { Shop, Item } = require("../src/gilded_rose")
 
 describe("Gilded Rose", function () {
   it("should foo", function () {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
-  });
-});
+    const gildedRose = new Shop([new Item("foo", 0, 0)])
+    const items = gildedRose.updateQuality()
+    expect(items[0].name).toBe("fixme")
+  })
+})
 
 /*
 DO NOT alter the Item class or the Item properties!
--- test/texttest_fixture.js
+++ test/texttest_fixture.js
@@ -1,5 +1,4 @@
-
-const { Shop, Item } = require("../src/gilded_rose");
+const { Shop, Item } = require("../src/gilded_rose")
 
 const items = [
   new Item("+5 Dexterity Vest", 10, 20),
@@ -13,15 +12,17 @@ const items = [
 
   // This Conjured item does not work properly yet
   new Item("Conjured Mana Cake", 3, 6),
-];
+]
 
-const days = Number(process.argv[2]) || 2;
-const gildedRose = new Shop(items);
+const days = Number(process.argv[2]) || 2
+const gildedRose = new Shop(items)
 
-console.log("OMGHAI!");
+console.log("OMGHAI!")
 for (let day = 0; day < days; day++) {
-  console.log(`\n-------- day ${day} --------`);
-  console.log("name, sellIn, quality");
-  items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
-  gildedRose.updateQuality();
+  console.log(`\n-------- day ${day} --------`)
+  console.log("name, sellIn, quality")
+  items.forEach((item) =>
+    console.log(`${item.name}, ${item.sellIn}, ${item.quality}`)
+  )
+  gildedRose.updateQuality()
 }
